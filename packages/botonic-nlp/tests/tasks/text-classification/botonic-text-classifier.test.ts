7366bc119127509f7317df4ec81a488f0306b65b
Eric Lozano <ericlozano98@gmail.com>
2021-04-19 17:26:00 +0200

    test(nlp): tests updated due to IndexedItems integration

--- packages/botonic-nlp/tests/tasks/text-classification/botonic-text-classifier.test.ts
+++ packages/botonic-nlp/tests/tasks/text-classification/botonic-text-classifier.test.ts
@@ -2,31 +2,22 @@ import { existsSync, rmdirSync } from 'fs'
 import { join } from 'path'
 
 import { DatabaseStorage } from '../../../src/embeddings/database/storage'
-import { PADDING_TOKEN, UNKNOWN_TOKEN } from '../../../src/preprocess/constants'
-import { STOPWORDS_EN } from '../../../src/preprocess/engines/en/stopwords-en'
 import { BotonicTextClassifier } from '../../../src/tasks/text-classification/botonic-text-classifier'
 import { TEXT_CLASSIFIER_TEMPLATE } from '../../../src/tasks/text-classification/models/types'
 import * as constantsHelper from '../../helpers/constants-helper'
 import * as toolsHelper from '../../helpers/tools-helper'
 
 describe('Botonic Text Classifier', () => {
-  const sut = new BotonicTextClassifier(
-    constantsHelper.LOCALE,
-    constantsHelper.MAX_SEQUENCE_LENGTH,
-    constantsHelper.CLASSES
-  )
-
-  test('Generate Vocabulary', () => {
-    const { trainSet } = toolsHelper.dataset.split()
-    sut.generateVocabulary(trainSet)
-    expect(sut.vocabulary.length).toBeGreaterThan(2)
-    expect(sut.vocabulary.includes(PADDING_TOKEN)).toBeTruthy()
-    expect(sut.vocabulary.includes(UNKNOWN_TOKEN)).toBeTruthy()
-  })
-
   test('Train and Evaluate model', async () => {
     const { trainSet, testSet } = toolsHelper.dataset.split()
-    sut.generateVocabulary(trainSet)
+    const vocabulary = trainSet.extractVocabulary(toolsHelper.preprocessor)
+    const sut = new BotonicTextClassifier(
+      constantsHelper.LOCALE,
+      constantsHelper.MAX_SEQUENCE_LENGTH,
+      constantsHelper.CLASSES,
+      vocabulary,
+      toolsHelper.preprocessor
+    )
     sut.compile()
     await sut.createModel(
       TEXT_CLASSIFIER_TEMPLATE.SIMPLE_NN,
@@ -43,8 +34,15 @@ describe('Botonic Text Classifier', () => {
   })
 
   test('Save Model', async () => {
-    const { trainSet } = toolsHelper.dataset.split(0.5, false)
-    sut.generateVocabulary(trainSet)
+    const { trainSet } = toolsHelper.dataset.split()
+    const vocabulary = trainSet.extractVocabulary(toolsHelper.preprocessor)
+    const sut = new BotonicTextClassifier(
+      constantsHelper.LOCALE,
+      constantsHelper.MAX_SEQUENCE_LENGTH,
+      constantsHelper.CLASSES,
+      vocabulary,
+      toolsHelper.preprocessor
+    )
     await sut.createModel(
       TEXT_CLASSIFIER_TEMPLATE.SIMPLE_NN,
       await DatabaseStorage.with(
@@ -58,16 +56,5 @@ describe('Botonic Text Classifier', () => {
     expect(existsSync(path)).toBeTruthy()
     rmdirSync(path, { recursive: true })
     expect(existsSync(path)).toBeFalsy()
-  })
-
-  test('Get Stopwords', () => {
-    const stopwords = sut.stopwords
-    expect(stopwords).toEqual(STOPWORDS_EN)
-  })
-
-  test('Set Stopwords', () => {
-    sut.stopwords = ['this', 'is']
-    const stopwords = sut.stopwords
-    expect(stopwords).toEqual(['this', 'is'])
   })
 })
