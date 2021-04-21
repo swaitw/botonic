f383532fa8bb6fadb5822ea07f37f7f29daf704c
Eric Lozano <ericlozano98@gmail.com>
2021-04-21 12:28:40 +0200

    test(plugin-ner): processor tests updated

--- packages/botonic-plugin-ner/tests/process/prediction-processor.test.ts
+++ packages/botonic-plugin-ner/tests/process/prediction-processor.test.ts
@@ -1,13 +1,50 @@
+import { PADDING_TOKEN } from '@botonic/nlp/lib/preprocess/constants'
+import { tensor, Tensor3D } from '@tensorflow/tfjs'
+
 import { PredictionProcessor } from '../../src/process/prediction-processor'
 import * as constantsHelper from '../helper/constants-helper'
 
 describe('Prediction processor', () => {
-  test('process prediction', () => {
-    const processor = new PredictionProcessor(constantsHelper.ENTITIES)
-    const sut = processor.process(
-      constantsHelper.SEQUENCE,
-      constantsHelper.PREDICTION
+  test('Process prediction', () => {
+    const sut = new PredictionProcessor(constantsHelper.ENTITIES)
+    const entities = sut.process(
+      [
+        'i',
+        'want',
+        'buy',
+        'pair',
+        'shoes',
+        PADDING_TOKEN,
+        PADDING_TOKEN,
+        PADDING_TOKEN,
+        PADDING_TOKEN,
+        PADDING_TOKEN,
+        PADDING_TOKEN,
+        PADDING_TOKEN,
+      ],
+      tensor([
+        [
+          [1, 0, 0, 0],
+          [1, 0, 0, 0],
+          [1, 0, 0, 0],
+          [1, 0, 0, 0],
+          [0, 1, 0, 0],
+          [1, 0, 0, 0],
+          [1, 0, 0, 0],
+          [1, 0, 0, 0],
+          [1, 0, 0, 0],
+          [1, 0, 0, 0],
+          [1, 0, 0, 0],
+          [1, 0, 0, 0],
+        ],
+      ]) as Tensor3D
     )
-    expect(sut.map(entity => entity.label)).toEqual(['O', 'O', 'product'])
+    expect(entities.map(entity => entity.label)).toEqual([
+      'O',
+      'O',
+      'O',
+      'O',
+      'product',
+    ])
   })
 })
